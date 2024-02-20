5be7acd65813515a64b961bd05cc9e9c28376e2d
parfait kouassi <parfaitkouess@gmail.com>
2024-02-20 13:33:33 -0500

    type: color add class FontColor for module color

---
+++ lib/styles/font_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FontColor {
+  static const blueLight = Color(0XFF252848);
+  static const darkBlue = Color(0XFF171B2F);
+  static const blue1 = Color(0XFF1D2137);
+  static const blue = Color(0XFF19A5D1);
+  static final greyLight = Colors.grey.withOpacity(0.6);
+  static const red = Color(0xFFD24545);
+}
