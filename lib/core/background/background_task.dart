b61c2664e3bdc1c5f431b46c8d9647274dbbdc54
parfait kouassi <parfaitkouess@gmail.com>
2024-03-15 10:47:19 -0400

    type: background task add background_task.dart

---
+++ lib/core/background/background_task.dart
@@ -0,0 +1,118 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:defi/core/enum.dart';
+import 'package:defi/core/hive_box_name.dart';
+import 'package:defi/core/notifications/setup_notification.dart';
+import 'package:defi/domain/entities/notification_crypto.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:hive/hive.dart';
+import 'package:intl/intl.dart';
+
+class BackgroundTask {
+  static final configNotification = SetupNotification(AwesomeNotifications());
+  
+  // Create notification not scheduled
+  static Future<void> createNotificationNotScheduled(
+      List cryptoMarketPrice, ServiceInstance instance) async {
+    var boxNotification = await Hive.openBox('notificationPriceBox');
+    List listNotifications = await boxNotification.get('notifications') ?? [];
+    await boxNotification.close();
+
+    // Convert notification to NotificationCrypto
+    final notificationEntities = listNotifications
+        .map((notif) => NotificationCrypto(
+            idNotification: notif["idNotification"],
+            cryptoId: notif["cryptoId"],
+            percent: notif['percent'],
+            typeNotification: AlertValue.values[notif['typeNotification']],
+            cron: notif["cron"],
+            image: notif["image"],
+            futurePrice: notif["futurePrice"]))
+        .toList();
+
+    // All notification that kind is not AlertValue.schedular
+    final notificationNotScheduled = notificationEntities
+        .where((notification) =>
+            notification.typeNotification != AlertValue.schedular)
+        .toList();
+
+    // create notification
+    for (var notification in notificationNotScheduled) {
+      final cryptoCurrentPrice = cryptoMarketPrice
+          .where((crypto) => crypto['id'] == notification.cryptoId)
+          .toList()[0];
+
+      // difference of price
+      final differencePrice =
+          (cryptoCurrentPrice['price'] - notification.futurePrice).abs();
+
+      // creat notification if diff is less 0.2
+      if (differencePrice >= 0 && differencePrice < 0.2) {
+        final createdThisDay =
+            await notificationIsTriggerThisDay(notification.idNotification);
+        if (!createdThisDay) {
+          // Display notification to screen
+          if (await configNotification.awesomeNotifications
+              .isNotificationAllowed()) {
+            configNotification.createAlertNotificationBasedPrice(notification);
+          }
+
+          // Register date of notification create
+          await createHistoryNotification(
+              notification.idNotification, instance);
+        }
+      } else {
+        //Logger().d('Notification not created');
+      }
+    }
+  }
+
+  
+
+  // Check if notification has already triggered this day
+  static Future<bool> notificationIsTriggerThisDay(int idNotification) async {
+    var boxHistoryNotification =
+        await Hive.openBox(HiveBoxName.notificationHistoryBox);
+    List history = await boxHistoryNotification.get(idNotification) ?? [];
+    await boxHistoryNotification.close();
+
+    if (history.isNotEmpty) {
+      // Format date is y/M/d
+      final currentDate = DateFormat.yMd().format(DateTime.now());
+      final lastNotificationDate =
+          DateFormat.yMd().format(history.last['date']);
+      return currentDate == lastNotificationDate;
+    }
+    return false;
+  }
+
+  // Log notification triggered
+  static Future<void> createHistoryNotification(
+      int idNotification, ServiceInstance instance) async {
+    var boxHistoryNotification =
+        await Hive.openBox(HiveBoxName.notificationHistoryBox);
+    List history = await boxHistoryNotification.get(idNotification) ?? [];
+
+    // insert to box
+    history.add({'date': DateTime.now(), 'open': false});
+    boxHistoryNotification.put(idNotification, history);
+
+    //Update number of notification active
+    int numberActiveNotification = 0;
+    for (var history in boxHistoryNotification.values) {
+      for (var logs in history) {
+        if (!logs['open']) {
+          numberActiveNotification += 1;
+        }
+      }
+    }
+    var boxCountNotificationBox =
+        await Hive.openBox(HiveBoxName.countNotificationBox);
+    boxCountNotificationBox.put('activeNotification', numberActiveNotification);
+
+    // Send number active notification to UI
+    instance.invoke('count', {'count': numberActiveNotification});
+
+    await boxCountNotificationBox.close();
+    await boxHistoryNotification.close();
+  }
+}
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -15,7 +15,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
-import 'package:logger/logger.dart';
 import '../../helpers/crypto_symbols.dart';
 import '../widget/card_balance.dart';
 import '../widget/theta_body_widget.dart';
