1b38a5b862f2e972ae5769945b636ae5bf62e86e
parfait kouassi <parfaitkouess@gmail.com>
2024-03-19 12:21:17 -0400

    type:impl repo implement repo for brightness

---
+++ lib/data/repositories/brightness_repo_impl.dart
@@ -0,0 +1,20 @@
+import 'package:defi/core/enum.dart';
+import 'package:defi/data/datasource/appearence_data.dart';
+import 'package:defi/domain/repositories/brightness/brightness_repo.dart';
+
+class BrightnessRepoImpl implements BrightnessRepo {
+  final BrightnessData brightnessData;
+
+  BrightnessRepoImpl(this.brightnessData);
+
+  @override
+  Future<Mode> getBrightness() async {
+    final mode = await brightnessData.getBrightness();
+    return mode;
+  }
+
+  @override
+  Future<void> changeBrightness() async {
+    await brightnessData.changeBrightness();
+  }
+}
