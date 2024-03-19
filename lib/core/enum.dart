c25b788d3177c7be9f953fde67756ed033befb81
parfait kouassi <parfaitkouess@gmail.com>
2024-03-19 12:04:11 -0400

    type:brigtness data layer complete data layer for brightness

--- lib/core/enum.dart
+++ lib/core/enum.dart
@@ -1,2 +1,5 @@
 // Kind of notification
 enum AlertValue { price, increase, decrease, schedular } 
+
+// Different appearence
+enum Mode {dark, light}
--- lib/core/hive_box_name.dart
+++ lib/core/hive_box_name.dart
@@ -2,4 +2,5 @@ class HiveBoxName {
   static const notificationHistoryBox = 'notificationHistoryBox';
   static const countNotificationBox = 'countNotificationBox';
   static const allNotificationTriggeredBox = 'allNotificationTriggeredBox';
+  static const settingsBox = "settingsBox";
 }
